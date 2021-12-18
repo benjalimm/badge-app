1eb3a603e6ce3c5234724fa9fba7f91c803e8981
Benjamin Lim <keymote@gmail.com>
2021-12-18 02:09:46 -0800

    Added basic login with eth

--- components/landingPage/NavBar.tsx
+++ components/landingPage/NavBar.tsx
@@ -1,12 +1,29 @@
+import { useWeb3React } from '@web3-react/core';
 import React from 'react';
 import styles from "../../styles/landingPage/lp.module.css";
+import { injected } from '../../services/injectedConnectors'
 
 export default function NavBar() {
+
+  const { active, account, library, connector, activate, deactivate } 
+  = useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className={styles.navBar}>
       <div className={styles.badgeLogo}>
         BADGE.
       </div>
+      <button className={styles.loginButton} onClick={connect}>
+        Sign in to Web3
+      </button>
     </div>
   )
 }
