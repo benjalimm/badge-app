641186cbc3537b820b33833521eed89b6138cd4d
Benjamin Lim <keymote@gmail.com>
2021-12-27 01:01:06 -0800

    Added basic code for ENS caching

--- components/navBar/AccountInfo.tsx
+++ components/navBar/AccountInfo.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import styles from "../../styles/navBar.module.css";
 import { ethers } from 'ethers';
+import cookieCutter from 'cookie-cutter';
+
+interface ENSStore { 
+  [key: string]: string;
+} 
+
 export default function AccountInfo({ account } : { account: string }) {
 
   const [walletHandle,setWalletHandle] = useState(null);
@@ -11,13 +17,49 @@ export default function AccountInfo({ account } : { account: string }) {
     return address.substring(0, 5) + ".." + address.substring(address.length - 3, address.length);
   }
 
+  const getENSFromCookies = (address: string): (string | undefined) => {
+    const ensStoreString = cookieCutter.get('walletENS');
+    if (ensStoreString) { 
+      const ensStore = JSON.parse(ensStoreString) as ENSStore;
+      if (ensStore) {
+        return ensStore[address];
+      }
+
+    }
+    
+  }
+  
+  const setENSInCookies = (address: string, ens: string) => {
+    const ensStoreString = cookieCutter.get('walletENS');
+    console.log("ENS Store: ", ensStoreString);
+    let ensStore = JSON.parse(ensStoreString) as ENSStore;
+
+    if (!ensStore) {
+      ensStore = { address: ens }
+      console.log(`ensStore: ${JSON.stringify(ensStore)}`)
+
+    } 
+
+    ensStore[address] = ens;
+
+    cookieCutter.set('walletENS', JSON.stringify(ensStore));
+  }
+
   useEffect(() => {
-    setWalletHandle(shortenAddress(account));
+    const ens = getENSFromCookies(account);
+
+    if (ens) {
+      setWalletHandle(ens);
+    } else {
+      setWalletHandle(shortenAddress(account));
+    }
+    
   },[])
 
   useEffect(() => {
     provider.lookupAddress(account).then(result => {
       setWalletHandle(result);
+      setENSInCookies(account, result);
     })
   },[])
   return (
