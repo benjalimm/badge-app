7e01af9f60a11f527324ad958a25337eb67f07d0
Benjamin Lim <keymote@gmail.com>
2021-12-29 01:02:19 -0800

    Added basic configs

---
+++ configs/blockchain.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  local : { 
+    url: 'http://localhost:8545',
+    badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+  },
+  mumbai: {
+    url: '',
+    badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+  },
+  mainnet: {
+    url: '',
+    badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+  }
+}
