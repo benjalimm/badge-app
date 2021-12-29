52ee1da2ebc0b774219a6aea832c9c955738b636
Benjamin Lim <keymote@gmail.com>
2021-12-28 21:27:00 -0800

    Not redownloading videos, added Dropbox links (#10)
    * Not redownloading videos, added Dropbox links
    
    * Removed next videos

--- components/landingPage/SampleBadgeCard.tsx
+++ components/landingPage/SampleBadgeCard.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
 import style from '../../styles/sampleBadgeCard.module.css';
-
+import cx from 'classnames';
 export default function SampleBadgeCard(
-  {  title, content, videoSource, profilePhotoSource }
+  {  title, content, videoSource, profilePhotoSource, visible }
   : { 
     title: string, 
     content: string, 
     videoSource: string, 
-    profilePhotoSource: string }
+    profilePhotoSource: string,
+    visible: boolean
+  }
 ) {
-  return <div className={style.sampleBadgeCard}>
+  const visibility = visible ? 'flex' : 'none';
+  return <div className={style.sampleBadgeCard} style={{ display : visibility}}>
     <div className={style.imageContainer}>
-      <video playsInline autoPlay loop muted src={videoSource}/>
+      <video preload="meta" autoPlay loop muted src={videoSource}/> 
     </div>
-      
     <div className={style.bottomContainer}>
       <img className={style.entityImage} src={profilePhotoSource}/>
       <h1 className={style.title}>
