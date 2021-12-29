1aea7093ee63107ee77b9fa849129fb71f3a2090
Benjamin Lim <keymote@gmail.com>
2021-12-29 15:15:53 -0800

    Added in blockchain config

--- configs/blockchain.config.js
+++ configs/blockchain.config.js
@@ -1,14 +1,15 @@
 module.exports = {
+  isLocal: true,
   local : { 
     url: 'http://localhost:8545',
     badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
   },
   mumbai: {
-    url: '',
+    url: 'https://polygon-mumbai.infura.io/v3/9c0e4231c73e40da8c90be9e43411cd6',
     badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
   },
   mainnet: {
-    url: '',
+    url: 'https://polygon-mainnet.infura.io/v3/9c0e4231c73e40da8c90be9e43411cd6',
     badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
   }
 }
