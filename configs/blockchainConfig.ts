908d7e799f3e27e77468f281352be3ee57d739a0
Benjamin Lim <keymote@gmail.com>
2021-12-30 00:27:01 -0800

    Update/deploy entity (#13)
    * Successfully deployed entity on local node
    
    * Added DeployEntitySuccessView, changed folders

--- configs/blockchainConfig.ts
+++ configs/blockchainConfig.ts
@@ -3,3 +3,4 @@ import config from './blockchain.config';
 export const isLocal = config.isLocal;
 export const isProd: boolean = process.env.IS_PROD == 'true'
 export const chainNetworkUrl = isProd ? config.mainnet.url : (isLocal ? config.local.url : config.mumbai.url);
+export const badgeContractAddress = isProd ? config.mainnet.badgeContractAddress : (isLocal ? config.local.badgeContractAddress : config.mumbai.badgeContractAddress);
