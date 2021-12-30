6ab20b66289fd5fea5e5ef82ff3e8d26ae394bf4
Benjamin Lim <keymote@gmail.com>
2021-12-29 23:41:20 -0800

    Successfully deployed entity on local node

--- configs/blockchain.config.js
+++ configs/blockchain.config.js
@@ -2,7 +2,7 @@ module.exports = {
   isLocal: true,
   local : { 
     url: 'http://localhost:8545',
-    badgeContractAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+    badgeContractAddress: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
   },
   mumbai: {
     url: 'https://polygon-mumbai.infura.io/v3/9c0e4231c73e40da8c90be9e43411cd6',
--- pages/genesis.tsx
+++ pages/genesis.tsx
@@ -1,15 +1,20 @@
 import Navbar from '../components/navBar/NavBar'
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import styles from '../styles/genesis.module.css'
 import PageTitleView from '../components/PageTitleView'
 import cx from 'classnames';
 import { useRouter } from 'next/router';
 import { Web3AuthContext } from '../contexts/Web3AuthContext';
+import { ethers } from 'ethers';
+import BadgeV1 from '../artifacts/contracts/BadgeV1.sol/BadgeV1.json';
+import GenesisToken from '../artifacts/contracts/GenesisToken.sol/GenesisToken.json';
+
+import { badgeContractAddress, chainNetworkUrl } from '../configs/blockchainConfig';
 
 export default function DeployEntityPage() {
-  
   const router = useRouter();
   const { active } = useContext(Web3AuthContext);
+
   useEffect(() => {
     if (!active) {
       router.push('/')
@@ -28,6 +33,37 @@ export default function DeployEntityPage() {
 }
 
 const DeployEntityEntryView = () => {
+
+  const [currentText, setCurrentText] =  useState<string>("");
+
+  const { web3Modal } = useContext(Web3AuthContext);
+
+  /**
+   * 
+   */
+  async function deployEntity() {
+    // const connection = await web3Modal.connect();
+    const provider = new ethers.providers.JsonRpcProvider()
+    const signer = provider.getSigner();
+    const badgeContract = new ethers.Contract(badgeContractAddress, BadgeV1.abi, signer);
+    const genesisTokenAddress = await badgeContract.genesisToken();
+
+    console.log("Genesis token contract ", genesisTokenAddress);
+    const genesisTokenContract = new ethers.Contract(genesisTokenAddress, GenesisToken.abi, signer);
+
+    const transaction = await genesisTokenContract.mintGenToken("tokenURI", currentText, badgeContractAddress);
+
+    await transaction.wait();
+    console.log("Transaction complete");
+  }
+
+  /**
+   * 
+   */
+  function onChange(event: React.FormEvent<HTMLInputElement>) {
+    setCurrentText(event.currentTarget.value);
+  }
+
   return <div className={styles.entryContainer}>
     <div className={cx(styles.entryContainerTopGradientSection)}>
       <div className={styles.noise}/>
@@ -36,10 +72,10 @@ const DeployEntityEntryView = () => {
       <h3 className={styles.formHeaderText}>Entity name</h3>
       <span className={styles.formHeaderSubtitleText}>(Don't worry, this is not permanent)</span>
       <div className={styles.formTextFieldContainer}>
-        <input className={styles.formTextField} type="text" placeholder="Entity name (e.g. Uniswap)"/>
+        <input className={styles.formTextField} type="text" placeholder="Entity name (e.g. Uniswap)" onChange={onChange}/>
       </div>
     </div>
-    <button className={styles.deployButton}>
+    <button className={styles.deployButton} onClick={deployEntity}>
         DEPLOY
     </button>
   </div>
