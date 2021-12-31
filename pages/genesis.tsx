1bf2454d80e5e868bfc3ed93e6e5cf4668b95711
Benjamin Lim <keymote@gmail.com>
2021-12-31 02:26:43 -0800

    Added in loading view, playing around with web3Modal

--- pages/genesis.tsx
+++ pages/genesis.tsx
@@ -6,13 +6,79 @@ import { useRouter } from 'next/router';
 import { Web3AuthContext } from '../contexts/Web3AuthContext';
 import DeployEntityEntryView from '../components/genesis/DeployEntityEntryView';
 import DeployEntitySuccessView from '../components/genesis/DeployEntitySuccessView';
+import DeployEntityLoadingView from '../components/genesis/DeployEntityLoadingView';
+import { EntityInfo } from '../schemas/genesis';
+import { ethers } from 'ethers';
+import BadgeV1 from '../artifacts/contracts/BadgeV1.sol/BadgeV1.json';
+import GenesisToken from '../artifacts/contracts/GenesisToken.sol/GenesisToken.json';
+import { badgeContractAddress } from '../configs/blockchainConfig';
+import Web3Modal from 'web3modal';
+import { chainNetworkUrl } from '../configs/blockchainConfig';
+type PageState = "ENTRY" | "LOADING" |"SUCCESS"
 
-type PageState = "ENTRY" | "SUCCESS"
+/**
+ * Genesis page
+ */
 export default function DeployEntityPage() {
   const router = useRouter();
   const { active } = useContext(Web3AuthContext);
-  const [pageState, setPageState] = useState<PageState>("SUCCESS")
+  const [pageState, setPageState] = useState<PageState>("ENTRY")
+  const [entityInfo, setEntityInfo] = useState<EntityInfo>({ 
+    address: "",
+    name: "",
+    genesisTokenHolder: ""
+  });
 
+  /**
+   * 
+   */
+  async function deployEntity(entityName: string) {
+
+    try {
+      setPageState("LOADING")
+      const web3Modal = new Web3Modal({
+        network: chainNetworkUrl, // optional
+        cacheProvider: true,
+      })
+      const connection = await web3Modal.connect
+      const provider = new ethers.providers.Web3Provider(connection)
+      const signer = provider.getSigner();
+
+      // Get genesis token contract address
+      const badgeContract = new ethers.Contract(badgeContractAddress, BadgeV1.abi, provider);
+      const genesisTokenAddress = await badgeContract.genesisToken();
+
+      // Initialize genesis token contract with signer 
+      const genesisTokenContract = new ethers.Contract(genesisTokenAddress, GenesisToken.abi, signer);
+
+      // Listen to EntityDeployed event
+      genesisTokenContract.once("EntityDeployed", (entityAddress: string, entityName: string, genesisTokenHolder: string) => {
+        console.log("Entity deployed ", entityAddress, entityName);
+        setEntityInfo({
+          address: entityAddress,
+          name: entityName,
+          genesisTokenHolder: genesisTokenHolder
+        })
+
+        if (pageState !== "SUCCESS") {
+          setPageState("SUCCESS");
+        }
+      
+      })
+
+      // Execute transaction to mint
+      const transaction = await genesisTokenContract.mintGenToken("tokenURI", entityName, badgeContractAddress);
+      await transaction.wait();
+
+    } catch (error) {
+      console.error(error)
+    }
+    
+  }
+
+  /**
+   * If the user is not logged in, redirect to landing page
+   */
   useEffect(() => {
     if (!active) {
       router.push('/')
@@ -22,9 +88,16 @@ export default function DeployEntityPage() {
   function renderViewBasedOnPageState(): ReactElement {
     switch (pageState) {
       case "ENTRY":
-        return <DeployEntityEntryView />
+        return <DeployEntityEntryView deployEntity={deployEntity}/>
+      case "LOADING":
+        return <DeployEntityLoadingView />
       case "SUCCESS":
-        return <DeployEntitySuccessView />
+        return <DeployEntitySuccessView 
+          name={entityInfo.name} 
+          address={entityInfo.address} 
+          genesisTokenHolder={entityInfo.genesisTokenHolder}
+          tokenHolderEnsName={entityInfo.tokenHolderEnsName}
+        />
       default:
         return <div>Unknown Page State</div>
     }
