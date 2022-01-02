db1019090f8f8aba0cba5d94be7742fa4d768b73
Benjamin Lim <keymote@gmail.com>
2022-01-01 23:07:41 -0800

    Update/genesisBigUpdate (#14)
    * Made changes
    
    * Added in loading view, playing around with web3Modal
    
    * Midway through fixing web3Modal

--- components/genesis/DeployEntitySuccessView.tsx
+++ components/genesis/DeployEntitySuccessView.tsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import { ethers } from 'ethers';
-import BadgeV1 from '../../artifacts/contracts/BadgeV1.sol/BadgeV1.json';
-import GenesisToken from '../../artifacts/contracts/GenesisToken.sol/GenesisToken.json';
-import { badgeContractAddress } from '../../configs/blockchainConfig';
+import React from 'react';
 import style from '../../styles/genesis.module.css'
-import cx from 'classnames';
+import { EntityInfo } from '../../schemas/genesis';
 
-const DeployEntitySuccessView = () => {
+const DeployEntitySuccessView = 
+({ name, address, genesisTokenHolder, tokenHolderEnsName }: EntityInfo) => {
 
   return <div className={style.entryContainer} style={{ height: '580px' }}>
+    <img className={style.blackBadge} src="/images/blackBadge.svg"/>
+    <h1 className={style.successHeader}>Entity successfully deployed on-chain</h1>
+    <h1 className={style.successSubheader}>Genesis token minted to your wallet</h1>
+    <div className={style.successEntityDetailsContainer}>
+      <h1 className={style.successEntityDetailsHeader}>Details</h1>
+      <EntitySuccessField 
+        title="Entity address" 
+        value={address}
+      />
+      <EntitySuccessField 
+        title="Entity name" 
+        value={name}
+      />
+      <EntitySuccessField 
+        title="Genesis token holder" 
+        value={genesisTokenHolder}
+      />
+    </div>
+    <button className={style.proceedButton}>Proceed</button>
+  </div>
+}
 
+const EntitySuccessField = ({ title, value } : { title: string, value: string}) => {
+  return <div className={style.successDetailsFieldContainer}>
+    <h1 className={style.successDetailsFieldHeader}>{title}</h1>
+    <span className={style.successDetailsFieldValue}>{value}</span>
   </div>
 }
 
