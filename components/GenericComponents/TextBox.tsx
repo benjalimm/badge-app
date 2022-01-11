3851a85c8937ba7ef313145c6a60bfec6db282e6
Benjamin Lim <keymote@gmail.com>
2022-01-10 22:58:08 -0800

    Update/mint badge view (#20)
    * Slowed down animation, pulling videos from within directory
    
    * Added most of mintBadgeView
    
    * Added transaction placeholders

--- components/GenericComponents/TextBox.tsx
+++ components/GenericComponents/TextBox.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from '../../styles/GenericComponents/textBox.module.css';
 
-export default function TextBox({ placeholder, onChange, customHeight, fontSize }: {
+export default function TextBox({ placeholder, onChange, customHeight, fontSize, value }: {
   onChange: (event: React.FormEvent<HTMLInputElement>) => void,
   placeholder: string;
   customHeight?: string;
   fontSize?: string;
+  value?: string;
 }) {
 
   const customStyle = {
@@ -13,6 +14,6 @@ export default function TextBox({ placeholder, onChange, customHeight, fontSize 
     fontSize: fontSize,
   }
   return <div className={style.formTextFieldContainer} style={customStyle}>
-    <input className={style.formTextField} type="text" placeholder={placeholder} onChange={onChange}/>
+    <input className={style.formTextField} type="text" placeholder={placeholder} onChange={onChange} value={value}/>
   </div>
 }
--- components/create/FormTextBoxContainer.tsx
+++ components/create/FormTextBoxContainer.tsx
@@ -3,15 +3,18 @@ import style from '../../styles/create/draftBadge.module.css'
 import TextBox from '../GenericComponents/TextBox';
 import TextArea from '../GenericComponents/TextArea';
 
-export default function FormTextBoxContainer({ type, title, placeholder, customTextBoxHeight, onChange }: { 
-  type: "TextBox" | "TextArea"
-  title: string, 
-  placeholder: string,
-  customTextBoxHeight?: string 
-  onChange:(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => void
-}) {
+export default function FormTextBoxContainer(
+  { type, title, placeholder, value, customTextBoxHeight, onChange, customStyle }: { 
+    type: "TextBox" | "TextArea"
+    title: string, 
+    placeholder: string,
+    value: string,
+    customTextBoxHeight?: string
+    onChange:(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+    customStyle?: React.CSSProperties
+  }) {
 
-  return <div className={style.formTextBoxContainer}>
+  return <div className={style.formTextBoxContainer} style={customStyle}>
     <h1 className={style.formTextBoxTitle}>{title}</h1>
     {
       (type ===  "TextBox") ? 
@@ -20,12 +23,14 @@ export default function FormTextBoxContainer({ type, title, placeholder, customT
           placeholder={placeholder} 
           customHeight={customTextBoxHeight}
           fontSize='13px'
+          value={value}
         /> :
         <TextArea
           onChange={onChange} 
           placeholder={placeholder} 
           customHeight={customTextBoxHeight}
           fontSize='13px'
+          value={value}
         />
     }  
   </div>
