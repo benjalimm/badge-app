fc586f95be3d5c5ae88f5b501534f8f20e5d9dd2
Benjamin Lim <keymote@gmail.com>
2022-01-13 23:16:28 -0800

    Removed pages, nav bar and changed sample badge data copy

--- utils/sampleCardData.ts
+++ utils/sampleCardData.ts
@@ -3,22 +3,22 @@ import { BadgeData } from "../schemas/BadgeData";
 const data: BadgeData[] = [
   {
     id: 0,
-    title: "Hackathon 2022",
-    content: "Harry won 'Most Innovative' for his project 'Picture This' for our Annual Hackathon in 2022.",
+    title: "Hackathon winner",
+    content: "Nathan won first place for his project 'Bullet' for the 2022 Annual Hackathon.",
     videoPath: "/videos/sampleNFTVideos/badgeKeyboard.mp4",
     profilePhotoSource: "/images/landingPage/coinbase_logo.jpeg"
   },
   {
     id: 1,
-    title: "DAO contributor",
-    content: "Lana's pull request for her implementation of the sign up page was approved and merged.",
+    title: "DAO core contributor",
+    content: "Lana was instrumental in architecting and building out the DAO's core contract. Additionally, she was heavily involved in the governance process.",
     videoPath: "/videos/sampleNFTVideos/badgeTick.mp4",
     profilePhotoSource: "/images/landingPage/uniswap_logo.jpeg"
   },
   {
     id: 2,
-    title: "Early investor",
-    content: "Jon was an early backer in the company. He not only invested his money but his time to help us get the product off the ground.",
+    title: "Investor and mentor",
+    content: "Jon not only led our seed round, but also spent countless hours helping us to design our customer onboarding.",
     videoPath: "/videos/sampleNFTVideos/badgeTrophy_1.mp4",
     profilePhotoSource: "/images/landingPage/opensea_logo.png"
   },
