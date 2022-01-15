ec86d86a7fb8a8e974684718d9696f6ca7e27b87
Benjamin Lim <keymote@gmail.com>
2022-01-14 19:40:51 -0800

    Changed to black and white favIcon

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,10 +22,10 @@ const LandingPage = () => {
   return (
     <div className={styles.lp}>
       <Head>
-        <title>Badge</title>
+        <title>Badge.</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="On-chain merit for internet organizations." />
-        <meta name="title" content="" />
+        <meta name="title" content="Badge." />
         <meta name="twitter:image" content="https://www.dropbox.com/s/d3y4bghdf7nexxe/TwitterImage.jpg?raw=1"/>
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@badgexyz"/>
@@ -34,8 +34,8 @@ const LandingPage = () => {
         <meta name="og:image" content="https://www.dropbox.com/s/d3y4bghdf7nexxe/TwitterImage.jpg?raw=1"/>
         <meta name="og:type" content="website"/>
         <meta name="og:url" content="https://badge.xyz"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/badgeFavIcon_16.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/badgeFavIcon_32.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/badgeWhiteFavIcon_16.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/badgeWhiteFavIcon_32.png"/>
 
       </Head>
       <Navbar sticky={false}/>
