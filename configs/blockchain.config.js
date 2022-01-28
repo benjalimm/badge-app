dac5e36cf53c37b2f044be32fbf0fdddd9f0a0be
Benjamin Lim <keymote@gmail.com>
2022-01-27 22:14:26 -0800

    Made changes (#36)

--- configs/blockchain.config.js
+++ configs/blockchain.config.js
@@ -2,7 +2,7 @@ module.exports = {
   isLocal: true,
   local : { 
     url: 'http://localhost:8545',
-    badgeContractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    badgeContractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
   },
   mumbai: {
     url: 'https://polygon-mumbai.infura.io/v3/9c0e4231c73e40da8c90be9e43411cd6',
