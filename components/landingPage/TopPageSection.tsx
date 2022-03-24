f4c385fc46700d6e47293f42467fd781c68d391a
Benjamin Lim <keymote@gmail.com>
2022-03-24 12:24:04 +1030

    Update/add second section (#46)
    * Implemented basic explanationPageSection
    
    * Implemented basic mobile optimized version
    
    * Added white noise and updated explanation images
    
    * Got bottom sign up to beta container fixed
    
    * Added footer
    
    * Made changes to explanationmargins

--- components/landingPage/TopPageSection.tsx
+++ components/landingPage/TopPageSection.tsx
@@ -26,12 +26,14 @@ export default function TopPageSection() {
   return (
     <div className={cx(style.lpPageSection, style.blueWhiteGradient)}>
       <div className={cx(style.topPageSectionContainer)}>
+        <div className={style.whiteNoise}/>
+      
         <div className={style.textContainer}>
           <h1 className={style.badgeHeaderText}>
           Achievements as NFTs.
           </h1>
           <h3 className={style.badgeNormalText}>
-            Infrastructure for on-chain reputation.
+            Protocol for on-chain reputation.
           </h3>
         </div>   
         <div className={style.cardContainer}>
@@ -47,7 +49,6 @@ export default function TopPageSection() {
       
           })
           }
- 
           <BottomTabs 
             numberOfTabs={cardData.length} 
             indexOfHighlightedTab={indexOfCurrentCard}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import Navbar from '../components/navBar/NavBar'
 import TopPageSection from '../components/landingPage/TopPageSection'
-import SecondPageSection from '../components/landingPage/SecondPageSection'
+import BottomPageSection from '../components/landingPage/BottomPageSection'
+import ExplanationPageSection from '../components/landingPage/ExplanationPageSection'
 import styles from '../styles/landingPage/lp.module.css'
 import React, { useEffect, useContext } from 'react'
 import { useRouter } from 'next/router';
 import { Web3AuthContext } from '../contexts/Web3AuthContext';
-import Head from 'next/head';
-
-const title = "Badge."
-const description = "On-chain merit for internet organizations."
-const largePreviewImage = "https://www.dropbox.com/s/1e7ns2g6qn4gkw2/walletEqualsResume.jpg?raw=1"
-
+import BadgeHead from '../components/landingPage/BadgeHead'
+import BadgeFooter from '../components/landingPage/BadgeFooter'
 const LandingPage = () => {
   const router = useRouter();
   const { active } = useContext(Web3AuthContext);
@@ -25,28 +22,14 @@ const LandingPage = () => {
 
   return (
     <div className={styles.lp}>
-      <Head>
-        <title>{title}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content={description} />
-        <meta name="title" content={title} />
-        <meta name="twitter:image" content={largePreviewImage}/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="@badgexyz"/>
-        <meta name="og:title" content={title}/>
-        <meta name="og:description" content={description}/>
-        <meta name="og:image" content={largePreviewImage}/>
-        <meta name="og:type" content="website"/>
-        <meta name="og:url" content="https://badge.xyz"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/badgeWhiteFavIcon_32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/badgeWhiteFavIcon_16.png"/>
-
-      </Head>
+      <BadgeHead/>
       <Navbar sticky={false}/>
       <div className={styles.sections}>
         <TopPageSection/>
-        <SecondPageSection/>
+        <ExplanationPageSection/>
+        <BottomPageSection/>
       </div>
+      <BadgeFooter/>
     </div>
   )
 }
