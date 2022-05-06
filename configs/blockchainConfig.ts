c6316281aefda54660c53790469f3fcbc0df5190
Benjamin Lim <keymote@gmail.com>
2022-05-06 18:33:23 +0930

    Update/addOptimism (#56)
    * Added config for optimism and optimistic kovan
    
    * Made chain easily variable
    
    * Set genesis starting state back to entry

--- configs/blockchainConfig.ts
+++ configs/blockchainConfig.ts
@@ -1,6 +1,24 @@
 import config from './blockchain.config';
-
+import { Chain } from '../schemas/ChainTypes'; 
 export const isLocal = config.isLocal;
 export const isProd: boolean = process.env.IS_PROD == 'true'
-export const chainNetworkUrl = isProd ? config.mainnet.url : (isLocal ? config.local.url : config.mumbai.url);
-export const badgeContractAddress = isProd ? config.mainnet.badgeContractAddress : (isLocal ? config.local.badgeContractAddress : config.mumbai.badgeContractAddress);
+
+// SET CURRENT CHAIN HERE
+export const currentChain: Chain = "Optimistic Kovan"
+export const getCurrentConfig = (chain: Chain) => {
+  switch (chain) {
+    case "Optimistic Kovan":
+      return config.optimisticKovan;
+    case "Polygon Mumbai":
+      return config.mumbai;
+    case "Optimistic Mainnet":
+      return config.optimismMainnet;
+    case "Polygon POS":
+      return config.mainnet;
+  }
+}
+
+export const currentConfig = getCurrentConfig(currentChain);
+
+export const chainNetworkUrl = isProd ? config.mainnet.url : (isLocal ? config.local.url : currentConfig.url);
+export const badgeContractAddress = isProd ? config.mainnet.badgeContractAddress : (isLocal ? config.local.badgeContractAddress : currentConfig.badgeContractAddress);
