4515b5a6427f9bc71ec7cc21d485d3047051605d
Benjamin Lim <keymote@gmail.com>
2022-05-21 01:06:07 -0700

    Added eth price oracle and amended transactions view

--- pages/create.tsx
+++ pages/create.tsx
@@ -31,6 +31,8 @@ export default function CreateBadgeView() {
   const [recipientAddress, setRecipientAddress] = useState<string | null>(null);
   const [email, setEmailAddress] = useState<string | null>(null);
   const [transactionHash, setTransactionHash] = useState<string>("");
+  const [estimatedGasFeesInEth, setEstimatedGasFeesInEth] = 
+  useState<number | null>(null)
 
   const { web3Modal } = useContext(Web3AuthContext);
 
@@ -60,6 +62,15 @@ export default function CreateBadgeView() {
 
   }, [pageState])
 
+  useEffect(() => {
+    estimateGasFees().then(fees => {
+      console.log
+      setEstimatedGasFeesInEth(fees)
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
+
   function getIndexOfCurrentStep(): number {
     return pageState === "DraftBadge" ? 0 : 1;
   }
@@ -74,7 +85,7 @@ export default function CreateBadgeView() {
     setPageState("DraftBadge");
   }
 
-  async function estimateGasFees() {
+  async function estimateGasFees(): Promise<number> {
 
     // 1. Establish connection
     const connection = await web3Modal.connect()
@@ -85,10 +96,10 @@ export default function CreateBadgeView() {
     console.log("Estimating gas...")
     // 2. Estimate gas 
     //** You should be able to enter in no ether with a level 0 Badge */
-    const estimation = await entity.estimateGas.mintBadge(recipientAddress, 0, "fakeURL", { value: 0})
+    const estimation = await entity.estimateGas.mintBadge("0x15eDb84992cd6E3ed4f0461B0Fbe743AbD1eA7b5", 0, "fakeURL", { value: 0})
     const etherEstimate = ethers.utils.formatEther(estimation)
     console.log(`Estimated gas: ${etherEstimate} ETH`)
-    alert(`Estimated gas: ${etherEstimate} ETH`)
+    return parseInt(etherEstimate);
   }
 
   async function onMintAndSendBadge(badgeData: BadgeData, recipientAddress: string, email?: string) {
@@ -183,6 +194,8 @@ export default function CreateBadgeView() {
           onMintAndSendBadge={onMintAndSendBadge}
           pageState={pageState}
           onBackToDraft={onBackToDraft}
+          gasFeesInEth={estimatedGasFeesInEth}
+          badgePriceInEth={0.05}
         />
 
     }
