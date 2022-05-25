668346ee2bbbdd02577fe92e32fbd0d9dcdcac56
Benjamin Lim <keymote@gmail.com>
2022-05-25 16:11:09 +0930

    Got basic SIWE wagmi working (#62)
    * Got basic SIWE wagmi working
    
    * Moved siwe to navbar
    
    * Removed web3 modal for transactions

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,27 @@ import TopPageSection from '../components/landingPage/TopPageSection'
 import BottomPageSection from '../components/landingPage/BottomPageSection'
 import ExplanationPageSection from '../components/landingPage/ExplanationPageSection'
 import styles from '../styles/landingPage/lp.module.css'
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router';
-import { Web3AuthContext } from '../contexts/Web3AuthContext';
 import BadgeHead from '../components/landingPage/BadgeHead'
 import BadgeFooter from '../components/landingPage/BadgeFooter'
+import { useSession, signOut } from "next-auth/react"
+import { useAccount, useConnect, useNetwork, useSignMessage } from 'wagmi'
+
 const LandingPage = () => {
   const router = useRouter();
-  const { active } = useContext(Web3AuthContext);
+  const { status } = useSession();
+  const active = (status === "authenticated");
 
   useEffect(() => {
 
     if (active) {
+      // signOut()
       router.push('/genesis')
     }
+    console.log(status)
   
-  }, [active])
+  }, [status])
 
   return (
     <div className={styles.lp}>
