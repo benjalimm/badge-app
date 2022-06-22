7ff37e907dd426d2566762786c9cc034e8c4190b
Benjamin Lim <keymote@gmail.com>
2022-06-21 22:55:45 -0700

    Added MeritLayerSection

---
+++ components/landingPage/FeaturesSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./FeaturesSection.module.css"
+
+export default function FeaturesSection() {
+  return <div>
+    <h1 className={styles.header}>Badge is a feature rich protocol:</h1>
+    <div className={styles.features}>
+
+    </div>
+  </div>
+}
+
+function Feature() {
+  return <div>
+
+  </div>
+}
