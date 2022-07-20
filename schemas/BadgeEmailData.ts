f32bcc308419557634f4d3beb985b2770e6a8621
Benjamin Lim <keymote@gmail.com>
2022-07-20 15:12:46 +0930

    Integrate in sendgrid + Badge emails (#63)
    * Got basic email api working
    
    * Updated email formatter
    
    * Reset package lock
    
    * Fix
    
    * Reinstalled node modules
    
    * made changes
    
    * Capitalized landing page
    
    * Send email
    
    * Changes made

---
+++ schemas/BadgeEmailData.ts
@@ -0,0 +1,10 @@
+export interface BadgeEmailData {
+  title: string;
+  content: string;
+  scanLink: string;
+  badgeLevel: number;
+  badgeXP: number;
+  entityName: string;
+  entityContractAddress: string;
+  recipientAddress: string;
+}
