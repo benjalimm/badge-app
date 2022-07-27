2cc6b9e9779b2bbcc277976b36c9f2d71cfcf872
Benjamin Lim <keymote@gmail.com>
2022-07-26 23:27:11 -0700

    Changes made

--- utils/hooks/useCurrentEntity.tsx
+++ utils/hooks/useCurrentEntity.tsx
@@ -16,16 +16,5 @@ export default function useCurrentEntity(): EntityInfo | undefined {
       EntityLocalStorageManager.stopListening(id);
     }
   },[])
-
-  // return {
-  //   address: "0x138bBfBB33eFD10619Bd62d6e047503FB487edd1",
-  //   name: "Test Badge labs",
-  //   genesisTokenHolder: "",
-  //   badgeToken: "",
-  //   permissionToken: "",
-  //   permissionTokenType: "GENESIS",
-  //   timestampOfLastVerified: 0,
-  //   chain: "Optimism Mainnet"
-  // }
   return currentEntity;
 }
