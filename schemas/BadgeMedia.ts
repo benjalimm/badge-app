c024a9654d6efe38324ebe8252b67655743c1122
Benjamin Lim <keymote@gmail.com>
2022-07-30 10:34:49 +0930

    Added permission token media list (#102)
    * Added permission token media list
    
    * Changes made

--- schemas/BadgeMedia.ts
+++ schemas/BadgeMedia.ts
@@ -1,8 +1,9 @@
-export interface BadgeMedia {
+export interface NFTMedia {
   id: number;
   name: string;
   storageUrl: string;
   quickAccessPath: string;
   gifUrl: string;
+  storageGif: string;
   mediaType: 'Video' | 'Image';
 }
