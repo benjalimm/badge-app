9a8dbbbcd25d2b484622c65695061f63121853fe
Benjamin Lim <keymote@gmail.com>
2022-08-03 22:34:24 -0700

    Add video to bottom part, changed one-liner

--- components/LandingPage/pageComponents/BadgeFooter.tsx
+++ components/LandingPage/pageComponents/BadgeFooter.tsx
@@ -4,7 +4,7 @@ export default function BadgeFooter() {
   return <footer className={style.footer}>
     <div className={style.textContainer}>
       <h1 className={style.badgeLogo}>BADGE.</h1>
-      <span className={style.subtitle}>Protocol for on-chain merit.</span>
+      <span className={style.subtitle}>A professional network that lives on-chain.</span>
       <span className={style.subtitle}>©2022 Badge Labs Inc. All rights reserved.</span>
     </div>
   </footer>
